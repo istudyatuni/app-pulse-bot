9658d9465ffb141154497cf72bd2b1efcb4dec15
Ilia <istudyatuni@gmail.com>
2023-11-11 16:14:59 +0300

    feat: simple dice bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use anyhow::Result;
+use dotenvy_macro::dotenv;
+use reqwest::Client;
+use teloxide::prelude::*;
+
+const TG_BOT_TOKEN: &str = dotenv!("BOT_TOKEN");
+const REQUEST_TIMEOUT_SEC: u64 = 30;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    pretty_env_logger::init();
+    log::info!("starting bot");
+    let bot = Bot::with_client(
+        TG_BOT_TOKEN,
+        Client::builder()
+            .timeout(Duration::from_secs(REQUEST_TIMEOUT_SEC))
+            .build()?,
+    );
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
+
+    Ok(())
 }

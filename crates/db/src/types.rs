67ef5aaab0aef81ade343680461c60e9d4293468
Ilia <istudyatuni@gmail.com>
2023-11-17 13:55:08 +0300

    refactor: use cast! for primitive db types

--- crates/db/src/types.rs
+++ crates/db/src/types.rs
@@ -14,42 +14,6 @@ impl Display for UserId {
     }
 }
 
-impl From<TgUserId> for UserId {
-    fn from(value: TgUserId) -> Self {
-        Self(value.0)
-    }
-}
-
-impl From<UserId> for TgUserId {
-    fn from(value: UserId) -> Self {
-        Self(value.0)
-    }
-}
-
-impl From<UserId> for TgChatId {
-    fn from(value: UserId) -> Self {
-        Self(value.0 as _)
-    }
-}
-
-impl From<UserId> for ChatId {
-    fn from(value: UserId) -> Self {
-        Self(value.0 as _)
-    }
-}
-
-impl From<UserId> for Id {
-    fn from(value: UserId) -> Self {
-        Self::Number(value.0 as _)
-    }
-}
-
-impl From<UserId> for Value {
-    fn from(value: UserId) -> Self {
-        Self::Number(Number::Int(value.0 as _))
-    }
-}
-
 #[derive(Debug, Default, Serialize, Deserialize, Clone, Copy)]
 #[serde(transparent)]
 pub struct ChatId(pub i64);
@@ -60,32 +24,30 @@ impl Display for ChatId {
     }
 }
 
-impl From<TgChatId> for ChatId {
-    fn from(value: TgChatId) -> Self {
-        Self(value.0)
-    }
+macro_rules! cast {
+    ($($from:ty => $to:ty : $value:ident => $convert:expr),* $(,)?) => {
+        $(impl From<$from> for $to {
+            fn from($value: $from) -> Self {
+                $convert
+            }
+        })*
+    };
 }
 
-impl From<TgChatId> for UserId {
-    fn from(value: TgChatId) -> Self {
-        Self(value.0 as _)
-    }
-}
+cast!(
+    ChatId => UserId: v => Self(v.0 as _),
+    UserId => ChatId: v => Self(v.0 as _),
 
-impl From<ChatId> for TgChatId {
-    fn from(value: ChatId) -> Self {
-        Self(value.0)
-    }
-}
+    ChatId => TgChatId: v => Self(v.0),
+    TgChatId => ChatId: v => Self(v.0),
 
-impl From<ChatId> for UserId {
-    fn from(value: ChatId) -> Self {
-        Self(value.0 as _)
-    }
-}
+    UserId => TgUserId: v => Self(v.0),
+    TgUserId => UserId: v => Self(v.0),
 
-impl From<ChatId> for Id {
-    fn from(value: ChatId) -> Self {
-        Self::Number(value.0)
-    }
-}
+    UserId => TgChatId: v => Self(v.0 as _),
+    TgChatId => UserId: v => Self(v.0 as _),
+
+    ChatId => Id: v => Self::Number(v.0),
+    UserId => Id: v => Self::Number(v.0 as _),
+    UserId => Value: v => Self::Number(Number::Int(v.0 as _)),
+);

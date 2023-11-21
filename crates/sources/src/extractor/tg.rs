715fff7217e5ecd82cfc54f698903a4102887516
Ilia <istudyatuni@gmail.com>
2023-11-21 21:02:25 +0300

    fix: show errors from telegram source

--- crates/sources/src/extractor/tg.rs
+++ crates/sources/src/extractor/tg.rs
@@ -14,12 +14,23 @@ pub(crate) async fn fetch_public_channel(name: &str) -> Result<Vec<Message>> {
         .await?
         .json()
         .await?;
+    if let Some(errors) = res.errors {
+        return Err(ResponseError::Arbitrary(errors).into());
+    }
     Ok(res.messages)
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Response {
+    #[serde(default = "Vec::new")]
     messages: Vec<Message>,
+    errors: Option<serde_json::Value>,
+}
+
+#[derive(Debug, thiserror::Error)]
+enum ResponseError {
+    #[error("got error when fetching: {0:?}")]
+    Arbitrary(serde_json::Value),
 }
 
 #[derive(Debug, Serialize, Deserialize)]

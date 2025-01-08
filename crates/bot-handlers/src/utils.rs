68219fe2528bb0dfb6ebac75b9521a9bc1cf438e
Ilia <istudyatuni@gmail.com>
2025-01-08 22:45:10 +0300

    update escape (copied from teloxide)

--- crates/bot-handlers/src/utils.rs
+++ crates/bot-handlers/src/utils.rs
@@ -1,22 +1,16 @@
 /// Modified version of [`teloxide::utils::markdown::escape`]
 pub(crate) fn escape<S: Into<String>>(s: S) -> String {
-    s.into()
-        // .replace('_', r"\_")
-        // .replace('*', r"\*")
-        .replace('[', r"\[")
-        .replace(']', r"\]")
-        .replace('(', r"\(")
-        .replace(')', r"\)")
-        .replace('~', r"\~")
-        .replace('`', r"\`")
-        .replace('>', r"\>")
-        .replace('#', r"\#")
-        .replace('+', r"\+")
-        .replace('-', r"\-")
-        .replace('=', r"\=")
-        .replace('|', r"\|")
-        .replace('{', r"\{")
-        .replace('}', r"\}")
-        .replace('.', r"\.")
-        .replace('!', r"\!")
+    const CHARS: [char; 16] = [
+        /*'_', '*',*/ '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}',
+        '.', '!',
+    ];
+
+    let s = s.into();
+    s.chars().fold(String::with_capacity(s.len()), |mut s, c| {
+        if CHARS.contains(&c) {
+            s.push('\\');
+        }
+        s.push(c);
+        s
+    })
 }

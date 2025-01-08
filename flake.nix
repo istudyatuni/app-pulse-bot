2b0465555bf9a339184421a90fd8cf2241939ace
Ilia <istudyatuni@gmail.com>
2025-01-08 21:46:37 +0300

    flake: set env for cross to use rootless docker

--- flake.nix
+++ flake.nix
@@ -14,14 +14,16 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
       in {
-        devShells.default = with pkgs; pkgs.mkShell {
-          nativeBuildInputs = [
-            just
-            # cargo-nextest
-            sqlite
-          ];
-          buildInputs = [];
-        };
+        devShells.default = with pkgs;
+          pkgs.mkShell {
+            nativeBuildInputs = [
+              just
+              # cargo-nextest
+              sqlite
+            ];
+            buildInputs = [];
+            CROSS_ROOTLESS_CONTAINER_ENGINE = 1;
+          };
         formatter = pkgs.alejandra;
       }
     );

9386c8f01ff37921f7a710bc27ecfa809c51371e
Ilia <istudyatuni@gmail.com>
2025-01-09 21:00:28 +0300

    sources: extract Update type

--- crates/sources/src/lib.rs
+++ crates/sources/src/lib.rs
@@ -2,15 +2,15 @@
 use std::time::Duration;
 
 use async_trait::async_trait;
-use reqwest::Url;
 use tokio::sync::mpsc::Sender;
-
-use common::UnixDateTime;
 
 pub mod alexstranniklite;
 
 mod extractor;
 mod timer;
+mod update;
+
+pub use update::*;
 
 pub(crate) const SOURCE_TIMEOUT: Duration = Duration::from_secs(60 * 60);
 
@@ -52,93 +52,6 @@ pub trait UpdateSource {
 pub enum UpdateSourceType {
     /// Source send all updates
     List,
-}
-
-#[derive(Debug, Default)]
-pub struct UpdatesList {
-    pub updates: Vec<Update>,
-    pub last_update: UnixDateTime,
-}
-
-impl UpdatesList {
-    pub fn is_empty(&self) -> bool {
-        self.updates.is_empty()
-    }
-    pub fn count(&self) -> usize {
-        self.updates.len()
-    }
-}
-
-#[derive(Debug, Default)]
-pub struct Update {
-    description: Option<String>,
-    description_link: Option<Url>,
-    update_link: Option<Url>,
-    app_id: String,
-    update_time: UnixDateTime,
-}
-
-impl Update {
-    pub(crate) fn builder() -> UpdateBuilder {
-        UpdateBuilder {
-            update: Update::default(),
-        }
-    }
-    pub fn app_id(&self) -> &str {
-        &self.app_id
-    }
-    pub fn description(&self) -> Option<&str> {
-        self.description.as_deref()
-    }
-    pub fn description_link(&self) -> &Option<Url> {
-        &self.description_link
-    }
-    pub fn update_link(&self) -> &Option<Url> {
-        &self.update_link
-    }
-    pub fn update_time(&self) -> UnixDateTime {
-        self.update_time
-    }
-}
-
-#[derive(Debug)]
-pub(crate) struct UpdateBuilder {
-    update: Update,
-}
-
-impl UpdateBuilder {
-    #[allow(unused)]
-    pub(crate) fn description<S: Into<String>>(mut self, description: S) -> Self {
-        self.update.description = Some(description.into());
-        self
-    }
-    /// `description_link` should be valid URL, otherwise it will be ignored
-    pub(crate) fn description_link(mut self, description_link: &str) -> Self {
-        let Ok(url) = Url::parse(description_link) else {
-            return self;
-        };
-        self.update.description_link = Some(url);
-        self
-    }
-    /// `update_link` should be valid URL, otherwise it will be ignored
-    pub(crate) fn update_link(mut self, update_link: &str) -> Self {
-        let Ok(url) = Url::parse(update_link) else {
-            return self;
-        };
-        self.update.update_link = Some(url);
-        self
-    }
-    pub(crate) fn app_id<S: Into<String>>(mut self, app_id: S) -> Self {
-        self.update.app_id = app_id.into();
-        self
-    }
-    pub(crate) fn update_time(mut self, update_time: UnixDateTime) -> Self {
-        self.update.update_time = update_time;
-        self
-    }
-    pub(crate) fn build(self) -> Update {
-        self.update
-    }
 }
 
 pub async fn start_update_loop<S>(source: S, tx: Sender<UpdatesList>)
---
+++ crates/sources/src/update.rs
@@ -0,0 +1,90 @@
+use reqwest::Url;
+
+use common::UnixDateTime;
+
+#[derive(Debug, Default)]
+pub struct UpdatesList {
+    pub updates: Vec<Update>,
+    pub last_update: UnixDateTime,
+}
+
+impl UpdatesList {
+    pub fn is_empty(&self) -> bool {
+        self.updates.is_empty()
+    }
+    pub fn count(&self) -> usize {
+        self.updates.len()
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Update {
+    description: Option<String>,
+    description_link: Option<Url>,
+    update_link: Option<Url>,
+    app_id: String,
+    update_time: UnixDateTime,
+}
+
+impl Update {
+    pub(crate) fn builder() -> UpdateBuilder {
+        UpdateBuilder {
+            update: Update::default(),
+        }
+    }
+    pub fn app_id(&self) -> &str {
+        &self.app_id
+    }
+    pub fn description(&self) -> Option<&str> {
+        self.description.as_deref()
+    }
+    pub fn description_link(&self) -> &Option<Url> {
+        &self.description_link
+    }
+    pub fn update_link(&self) -> &Option<Url> {
+        &self.update_link
+    }
+    pub fn update_time(&self) -> UnixDateTime {
+        self.update_time
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct UpdateBuilder {
+    update: Update,
+}
+
+impl UpdateBuilder {
+    #[allow(unused)]
+    pub(crate) fn description<S: Into<String>>(mut self, description: S) -> Self {
+        self.update.description = Some(description.into());
+        self
+    }
+    /// `description_link` should be valid URL, otherwise it will be ignored
+    pub(crate) fn description_link(mut self, description_link: &str) -> Self {
+        let Ok(url) = Url::parse(description_link) else {
+            return self;
+        };
+        self.update.description_link = Some(url);
+        self
+    }
+    /// `update_link` should be valid URL, otherwise it will be ignored
+    pub(crate) fn update_link(mut self, update_link: &str) -> Self {
+        let Ok(url) = Url::parse(update_link) else {
+            return self;
+        };
+        self.update.update_link = Some(url);
+        self
+    }
+    pub(crate) fn app_id<S: Into<String>>(mut self, app_id: S) -> Self {
+        self.update.app_id = app_id.into();
+        self
+    }
+    pub(crate) fn update_time(mut self, update_time: UnixDateTime) -> Self {
+        self.update.update_time = update_time;
+        self
+    }
+    pub(crate) fn build(self) -> Update {
+        self.update
+    }
+}

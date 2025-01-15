d11d1a6d29159da0a62191fc468d7841f62a7d8b
Ilia <istudyatuni@gmail.com>
2025-01-15 21:09:18 +0300

    common: move test helpers under "test" feature

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,11 +1,15 @@
 mod datetime;
 mod env;
 mod log;
-mod test_logger;
 mod tokio;
+
+#[cfg(feature = "test")]
+mod test_logger;
 
 pub use datetime::*;
 pub use env::*;
 pub use log::*;
-pub use test_logger::*;
 pub use tokio::*;
+
+#[cfg(feature = "test")]
+pub use test_logger::*;
